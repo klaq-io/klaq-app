ecefa8fe8c5dbfb15081376b6afebc3edcf5d118
Paul <paul.fermey@epitech.eu>
2023-05-03 19:29:50 -0700

    fix: resize

--- src/App.tsx
+++ src/App.tsx
@@ -4,9 +4,46 @@ import { Dashboard, Login } from "./pages";
 import { PATHS } from "./routes";
 import PrivateRoute from "./utils/PrivateRoute";
 import { ForgetPassword } from "./pages/ForgetPassword/ForgetPassword";
+import { useState, useEffect } from "react";
+
+const MINIMUM_SCREEN_SIZE = {
+  width: 1024,
+  height: 690,
+};
 
 function App() {
-  return (
+  const [dimensions, setDimensions] = useState({
+    height: window.innerHeight,
+    width: window.innerWidth,
+  });
+
+  useEffect(() => {
+    function handleResize() {
+      setDimensions({
+        height: window.innerHeight,
+        width: window.innerWidth,
+      });
+    }
+    window.addEventListener("resize", handleResize);
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  });
+
+  return dimensions.width <= MINIMUM_SCREEN_SIZE.width ||
+    dimensions.height <= MINIMUM_SCREEN_SIZE.height ? (
+    <div className="flex justify-center bg-primary h-full">
+      <div className="card w-96 bg-base-100 shadow-xl m-6">
+        <div className="card-body items-center text-center">
+          <h2 className="card-title">Oups!</h2>
+          <p className="text-xl">
+            Please use a computer to use{" "}
+            <span className="font-bold">Klaq.io</span>
+          </p>
+        </div>
+      </div>
+    </div>
+  ) : (
     <BrowserRouter>
       <Routes>
         <Route path={PATHS.SIGN_UP} element={<div>SignUp</div>} />
--- src/layouts/PageLayout/PageLayout.tsx
+++ src/layouts/PageLayout/PageLayout.tsx
@@ -6,45 +6,10 @@ type Props = {
   children: string | JSX.Element | JSX.Element[];
 };
 
-const MINIMUM_SCREEN_SIZE = {
-  width: 1024,
-  height: 690,
-};
-
 export const PageLayout = (props: Props) => {
   const { children } = props;
 
-  const [dimensions, setDimensions] = useState({
-    height: window.innerHeight,
-    width: window.innerWidth,
-  });
-  useEffect(() => {
-    function handleResize() {
-      setDimensions({
-        height: window.innerHeight,
-        width: window.innerWidth,
-      });
-    }
-    window.addEventListener("resize", handleResize);
-    return () => {
-      window.removeEventListener("resize", handleResize);
-    };
-  });
-
-  return dimensions.width <= MINIMUM_SCREEN_SIZE.width ||
-    dimensions.height <= MINIMUM_SCREEN_SIZE.height ? (
-    <div className="flex justify-center bg-primary h-full">
-      <div className="card w-96 bg-base-100 shadow-xl m-6">
-        <div className="card-body items-center text-center">
-          <h2 className="card-title">Oups!</h2>
-          <p className="text-xl">
-            Please use a computer to use{" "}
-            <span className="font-bold">Klaq.io</span>
-          </p>
-        </div>
-      </div>
-    </div>
-  ) : (
+  return (
     <main className="relative h-screen overflow-hidden bg-gray-100 ">
       <div className="flex items-start justify-between">
         <div className="relative hidden h-screen lg:block w-60">
