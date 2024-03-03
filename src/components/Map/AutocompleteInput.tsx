0bcb631b275520d9319257f6469e90d449be6b15
Paul <paul.fermey@epitech.eu>
2024-03-03 21:40:54 +0100

    remove useless console log

--- src/components/Map/AutocompleteInput.tsx
+++ src/components/Map/AutocompleteInput.tsx
@@ -54,7 +54,6 @@ export const MapAutocompleteInput: FC<MapAutocompleteInputProps> = (
           className="w-full rounded-md border-0 bg-white py-1.5 pl-10 pr-10 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-klaq-600 sm:text-sm sm:leading-6"
           onChange={(event) => fetchSuggestions(event.target.value)}
           displayValue={(suggestion: AddressSuggestion) => {
-            console.log(suggestion);
             return suggestion !== null && suggestion.full_address
               ? suggestion.full_address
               : suggestion.name
